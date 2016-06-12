b9d23b6c2de2f2607c7045b72a01d26c32a5d6e3
Dry0Gut <moralejov@gmail.com>
2016-06-12 10:15:19 -0500

    Configuración inicial del proyecto Almacen

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Almacen\Http\Controllers;
+
+use Almacen\Http\Requests;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
